08bb19dbad466af66012bb9cb55ed9c6072bcd3c
douglas-augusto <douglas-augusto@hotmail.com>
2017-09-28 15:36:24 -0300

    Tratamento de exceções para logar usuário

--- app/src/main/java/com/desopilando/aliatech/desopilando/activity/LoginActivity.java
+++ app/src/main/java/com/desopilando/aliatech/desopilando/activity/LoginActivity.java
@@ -16,6 +16,8 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -67,16 +69,40 @@ public class LoginActivity extends AppCompatActivity {
 
                 if(task.isSuccessful()){
 
+                    abrirTelaPrincipal();
                     Toast.makeText(LoginActivity.this, "Logado com sucesso", Toast.LENGTH_LONG).show();
 
                 }else{
 
-                    Toast.makeText(LoginActivity.this, "Erro ao fazer login", Toast.LENGTH_LONG).show();
+                    String erroExcecao = "";
+
+                    try{
+
+                       throw task.getException();
+
+                    } catch (FirebaseAuthInvalidUserException e) {
+                        erroExcecao = "Essa conta de usuário não existe ou está desativada!";
+                    } catch (FirebaseAuthInvalidCredentialsException e) {
+                        erroExcecao = "Verifique seu e-mail e senha!";
+                    } catch (Exception e) {
+                        erroExcecao = "Não foi possível fazer o login!";
+                        e.printStackTrace();
+                    }
+
+                    Toast.makeText(LoginActivity.this, "Erro: "+erroExcecao, Toast.LENGTH_LONG).show();
 
                 }
 
             }
         });
+
+    }
+
+    private void abrirTelaPrincipal(){
+
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
 
     }
 
