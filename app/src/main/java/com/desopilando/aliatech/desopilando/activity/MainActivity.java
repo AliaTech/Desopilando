7be0753bf4868066c0ee5ac7aa0a50b32e43def9
douglas-augusto <douglas-augusto@hotmail.com>
2017-09-28 22:30:02 -0300

    Finalizando toolbar + ajuste de cores

--- app/src/main/java/com/desopilando/aliatech/desopilando/activity/MainActivity.java
+++ app/src/main/java/com/desopilando/aliatech/desopilando/activity/MainActivity.java
@@ -1,21 +1,28 @@
 package com.desopilando.aliatech.desopilando.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.desopilando.aliatech.desopilando.R;
+import com.desopilando.aliatech.desopilando.activity.config.ConfiguracaoFirebase;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private FirebaseAuth usuarioAutenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        usuarioAutenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("Desopilando");
@@ -28,5 +35,28 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+
+            case R.id.item_sair:
+                deslogarUsuario();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    public void deslogarUsuario(){
+
+        usuarioAutenticacao.signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+
     }
 }
