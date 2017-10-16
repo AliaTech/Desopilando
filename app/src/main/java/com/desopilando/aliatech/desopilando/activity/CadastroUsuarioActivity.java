679ce4f9f7cb88d08b74fdcc2eca3b1983db48ad
douglas-augusto <douglas-augusto@hotmail.com>
2017-10-15 21:22:38 -0300

    Base64

--- app/src/main/java/com/desopilando/aliatech/desopilando/activity/CadastroUsuarioActivity.java
+++ app/src/main/java/com/desopilando/aliatech/desopilando/activity/CadastroUsuarioActivity.java
@@ -1,5 +1,6 @@
 package com.desopilando.aliatech.desopilando.activity;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,6 +11,7 @@ import android.widget.Toast;
 
 import com.desopilando.aliatech.desopilando.R;
 import com.desopilando.aliatech.desopilando.activity.config.ConfiguracaoFirebase;
+import com.desopilando.aliatech.desopilando.activity.helper.Base64Custom;
 import com.desopilando.aliatech.desopilando.activity.model.Usuario;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -85,12 +87,12 @@ public class CadastroUsuarioActivity extends AppCompatActivity {
 
                     //Salvar os dados do usuario no banco
                     FirebaseUser usuarioFirebase = task.getResult().getUser();
-                    usuario.setId(usuarioFirebase.getUid());
+                    String identificadorUsuario = Base64Custom.codificarBase64(usuario.getEmail());
+
+                    usuario.setId(identificadorUsuario);
                     usuario.salvar();
 
-                    //Após o cadastro usuario é deslogado e retorna a tela de login
-                    autenticacao.signOut();
-                    finish();
+                    abrirLoginUsuario();
 
                 }else{
                     //Tratamento de exceções
@@ -114,6 +116,14 @@ public class CadastroUsuarioActivity extends AppCompatActivity {
                 }
             }
         });
+
+    }
+
+    public void abrirLoginUsuario(){
+
+        Intent intent = new Intent(CadastroUsuarioActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
 
     }
 }
---
+++ app/src/main/java/com/desopilando/aliatech/desopilando/activity/helper/Base64Custom.java
@@ -0,0 +1,23 @@
+package com.desopilando.aliatech.desopilando.activity.helper;
+
+import android.util.Base64;
+
+/**
+ * Created by douglas on 15/10/17.
+ */
+
+public class Base64Custom {
+
+    public static String codificarBase64(String texto){
+
+        return Base64.encodeToString(texto.getBytes(), Base64.DEFAULT).replaceAll("(\\n|\\r)","");
+
+    }
+
+    public static String decodificarBase64(String textoCodificado){
+
+        return  new String(Base64.decode(textoCodificado, Base64.DEFAULT));
+
+    }
+
+}
